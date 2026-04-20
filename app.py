64651af337379e85d0b2994ad962093cbe8bf814
Hamza Shahbaz <121577494+HamzaShahbaz0512@users.noreply.github.com>
2026-04-21 01:23:33 +0500

    tested from post man we can make post and get call

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
+class Task:
+    def __init__(self,name,depend_on=None,Difficulty=None,status="pending"):
+        self.name=name
+        self.depend_on=depend_on
+        self.difficulty=Difficulty
+        self.status=status
+        
+tasks = {}
+
 @app.route('/tasks',methods=['GET'])
 def get_tasks():
-    return jsonify({'message':['Hello world']})
+    result = {}
+    for name, task in tasks.items():
+        result[name] = {
+            "name": task.name,
+            "depend_on": task.depend_on,
+            "difficulty": task.difficulty,
+            "status": task.status
+        }
+    return jsonify(result)
+
+@app.route('/tasks',methods=['POST'])
+def add_Task():
+    data = request.get_json()
+    name=data.get('name')
+    if name in tasks:
+        return jsonify({'error':'Task already exists'}) ,400
+    tasks[name] = Task(name)
+    return jsonify({'message': f'Task {name} added'}), 201
+
 
 if(__name__=='__main__'):
     app.run(debug=True)
