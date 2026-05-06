6e692b81a69fade655543c8bf22fe148e391a52c
Hamza Shahbaz <121577494+HamzaShahbaz0512@users.noreply.github.com>
2026-05-07 01:31:48 +0500

    Implemented end point for put get all items and get specifi item and delete a task

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask,jsonify,request
 app = Flask(__name__)
 
 class Task:
-    def __init__(self,name,depend_on=None,Difficulty=None,status="pending"):
+    def __init__(self,id,name,depend_on=None,Difficulty=None,status="pending"):
+        self.id=id
         self.name=name
         self.depend_on=depend_on
         self.difficulty=Difficulty
@@ -11,11 +12,14 @@ class Task:
         
 tasks = {}
 
+#Get all items
+
 @app.route('/tasks',methods=['GET'])
 def get_tasks():
     result = {}
-    for name, task in tasks.items():
-        result[name] = {
+    for id, task in tasks.items():
+        result[id] = {
+            "id":task.id,
             "name": task.name,
             "depend_on": task.depend_on,
             "difficulty": task.difficulty,
@@ -23,15 +27,60 @@ def get_tasks():
         }
     return jsonify(result)
 
+#Create an item
+
 @app.route('/tasks',methods=['POST'])
 def add_Task():
     data = request.get_json()
+    id=data.get('id')
     name=data.get('name')
-    if name in tasks:
+    if id in tasks:
         return jsonify({'error':'Task already exists'}) ,400
-    tasks[name] = Task(name)
-    return jsonify({'message': f'Task {name} added'}), 201
+    tasks[id] = Task(id,name)
+    tasks[id].depend_on=data.get("depend_on")
+    tasks[id].difficulty=data.get("difficulty")
+    tasks[id].status=data.get("status")
+    return jsonify({'message': f'Task {id} added'}), 201
 
+#Update a specific item
+@app.route('/tasks/<id>',methods=['PUT'])
+
+def update_task(id):
+    data=request.get_json()
+    if id not in tasks:
+        return jsonify({'error':'Task not found'}), 404
+    
+    if data.get('name'):
+        tasks[id].name = data.get('name')
+    if data.get('depend_on'):
+        tasks[id].depend_on = data.get('depend_on')
+    if data.get('difficulty'):
+        tasks[id].difficulty = data.get('difficulty')
+    if data.get('status'):
+        tasks[id].status = data.get('status')
+    return jsonify({'message': f'Task {id} updated'})
+
+#Get a specific item
+@app.route('/tasks/<id>',methods=['GET'])
+
+def get_item(id):
+    if id not in tasks:
+        return jsonify({'error':'Task not found'}), 404
+    data1={
+        "id": tasks[id].id,
+        "name": tasks[id].name,
+        "depend_on": tasks[id].depend_on,
+        "difficulty": tasks[id].difficulty,
+        "status": tasks[id].status
+    }
+    return jsonify(data1)
+@app.route('/tasks/<id>',methods=['DELETE'])
+
+def delete_task(id):
+    if id not in tasks:
+        return jsonify({'error':'Task not found'}), 404
+    del tasks[id]
+    return jsonify({'message': f'Task {id} deleted'})
 
 if(__name__=='__main__'):
     app.run(debug=True)
